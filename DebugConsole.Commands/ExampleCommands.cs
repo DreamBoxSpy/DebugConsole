59a68994d4c7c4600719e2caa5c754652ab7c621
DreamBoxSpy <156294353+DreamBoxSpy@users.noreply.github.com>
2026-02-04 21:04:22 +0800

    chore: upload

---
+++ DebugConsole.Commands/ExampleCommands.cs
@@ -0,0 +1,22 @@
+﻿using IngameDebugConsole;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DebugConsole.Commands
+{
+    internal static class ExampleCommands
+    {
+        [ConsoleMethod("test", "", "msg")]
+        public static void Echo(TextWriter writer, string message)
+        {
+            writer.WriteLine(message);
+        }
+        [ConsoleMethod("exit-example", "Exit the game.")]
+        public static void ExitGame(TextWriter writer)
+        {
+            writer.WriteLine("Exiting game...");
+            Environment.Exit(0);
+        }
+    }
+}
